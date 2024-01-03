6fc03205954759a653c93bae5d3d7e8d4208159a
Élveson Costa <elveson.esc@gmail.com>
2024-01-03 10:51:09 -0300

    initial commit

---
+++ android/app/src/main/kotlin/com/sciire/app_pokedex/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sciire.app_pokedex
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:app_pokedex/src/app_pokedex.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const AppPokedex(),
+  );
+}
